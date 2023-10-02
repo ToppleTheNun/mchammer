313e0659cb2e5d4a3158cc7377b07a307295f195
Richard Harrah <topplethenunnery@gmail.com>
2023-10-02 00:41:05 -0400

    update logo

--- app/root.tsx
+++ app/root.tsx
@@ -51,29 +51,29 @@ export const links: LinksFunction = () => {
     {
       rel: "apple-touch-icon",
       sizes: "180x180",
-      href: "/apple-touch-icon.png",
+      href: "/apple-touch-icon.png?v=1",
     },
     {
       rel: "icon",
       type: "image/png",
       sizes: "32x32",
-      href: "/favicon-32x32.png",
+      href: "/favicon-32x32.png?v=1",
     },
     {
       rel: "icon",
       type: "image/png",
       sizes: "16x16",
-      href: "/favicon-16x16.png",
+      href: "/favicon-16x16.png?v=1",
     },
     {
       rel: "icon",
       type: "image/x-icon",
-      href: "/favicon.ico",
+      href: "/favicon.ico?v=1",
     },
-    { rel: "manifest", href: "/site.webmanifest" },
+    { rel: "manifest", href: "/site.webmanifest?v=1" },
     {
       rel: "mask-icon",
-      href: "/safari-pinned-tab.svg",
+      href: "/safari-pinned-tab.svg?v=1",
       color: "#5bbad5",
     },
 
