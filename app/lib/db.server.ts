3a0f25ffd4867ac39ee557a8381e28bb75249ac1
Richard Harrah <topplethenunnery@gmail.com>
2023-10-12 18:56:09 -0400

    comment out prisma for now

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,7 +1,7 @@
 import { createPool } from "@vercel/postgres";
 import { drizzle as drizzleVercelPostgresAdapter } from "drizzle-orm/vercel-postgres";
 
-import { PrismaClient } from "#app/generated/prisma/index.js";
+// import { PrismaClient } from "#app/generated/prisma/index.js";
 import * as schema from "#app/lib/db/schema.ts";
 import { singleton } from "#app/lib/singleton.server.ts";
 
@@ -11,7 +11,7 @@ export const drizzle = singleton("drizzle", () =>
   drizzleVercelPostgresAdapter(db, { schema }),
 );
 
-export const prisma = singleton(
-  "prisma",
-  () => new PrismaClient({ log: ["query", "info", "warn", "error"] }),
-);
+// export const prisma = singleton(
+//   "prisma",
+//   () => new PrismaClient({ log: ["query", "info", "warn", "error"] }),
+// );
--- app/lib/monitoring.server.ts
+++ app/lib/monitoring.server.ts
@@ -1,12 +1,12 @@
 import * as Sentry from "@sentry/remix";
 
-import { prisma } from "#app/lib/db.server.ts";
+// import { prisma } from "#app/lib/db.server.ts";
 
 export function init() {
   Sentry.init({
     dsn: ENV.SENTRY_DSN,
     environment: ENV.VERCEL_ENV,
     tracesSampleRate: ENV.MODE === "production" ? 0.2 : 1.0,
-    integrations: [new Sentry.Integrations.Prisma({ client: prisma })],
+    // integrations: [new Sentry.Integrations.Prisma({ client: prisma })],
   });
 }
