c7589c4025f59464fe0dd91e6917a50bb977cc4a
Richard Harrah <topplethenunnery@gmail.com>
2023-10-12 17:48:08 -0400

    prep for switching to prisma

--- app/ingest/log.server.ts
+++ app/ingest/log.server.ts
@@ -59,3 +59,54 @@ export const ingestWarcraftLogsReport = async (
     dodgeParryMissStreaks.dodgeParryMissStreaks.length,
   );
 };
+
+export const ingestWarcraftLogsReportPrisma = async (
+  reportCode: string,
+  timings: Timings,
+) => {
+  info(`Ingesting WCL report: ${reportCode}`);
+  // let fights: ReportWithIngestedFights;
+  // try {
+  //   fights = await ingestFightsFromReport(reportCode, timings);
+  // } catch (e) {
+  //   error(`Unable to ingest WCL report: ${reportCode}`, e);
+  //   return;
+  // }
+  // debug(
+  //   `Ingested fights from ${reportCode}:`,
+  //   fights.fights.map((fight) => fight.id)?.join(", "),
+  // );
+  //
+  // let damageTakenEvents: ReportWithIngestedDamageTakenEvents;
+  // try {
+  //   damageTakenEvents = await ingestDamageTakenEvents(fights, timings);
+  // } catch (e) {
+  //   error(
+  //     `Unable to ingest WCL damage taken events for report: ${reportCode}`,
+  //     e,
+  //   );
+  //   return;
+  // }
+  // debug(
+  //   `Ingested damage taken events from ${reportCode}: `,
+  //   damageTakenEvents.damageTakenEvents.length,
+  // );
+  //
+  // let dodgeParryMissStreaks: ReportWithIngestedDodgeParryMissStreaks;
+  // try {
+  //   dodgeParryMissStreaks = await ingestDodgeParryMissStreaks(
+  //     damageTakenEvents,
+  //     timings,
+  //   );
+  // } catch (e) {
+  //   error(
+  //     `Unable to ingest dodge/parry/miss streaks for report: ${reportCode}`,
+  //     e,
+  //   );
+  //   return;
+  // }
+  // debug(
+  //   `Ingested dodge/parry/miss streaks from ${reportCode}: `,
+  //   dodgeParryMissStreaks.dodgeParryMissStreaks.length,
+  // );
+};
--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,6 +1,7 @@
 import { createPool } from "@vercel/postgres";
 import { drizzle as drizzleVercelPostgresAdapter } from "drizzle-orm/vercel-postgres";
 
+import { PrismaClient } from "#app/generated/prisma/index.js";
 import * as schema from "#app/lib/db/schema.ts";
 import { singleton } from "#app/lib/singleton.server.ts";
 
@@ -8,4 +9,9 @@ export const db = singleton("db", () => createPool());
 
 export const drizzle = singleton("drizzle", () =>
   drizzleVercelPostgresAdapter(db, { schema }),
+);
+
+export const prisma = singleton(
+  "prisma",
+  () => new PrismaClient({ log: ["query", "info", "warn", "error"] }),
 );
