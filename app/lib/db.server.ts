af35fdfa67cf46eb0f0e8eebe353d51d9ba4331c
Richard Harrah <topplethenunnery@gmail.com>
2023-10-22 13:58:25 -0400

    rip prisma back out

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,7 +1,6 @@
 import { createPool } from "@vercel/postgres";
 import { drizzle as drizzleVercelPostgresAdapter } from "drizzle-orm/vercel-postgres";
 
-// import { PrismaClient } from "#app/generated/prisma/index.js";
 import * as schema from "#app/lib/db/schema.ts";
 import { singleton } from "#app/lib/singleton.server.ts";
 
@@ -10,8 +9,3 @@ export const db = singleton("db", () => createPool());
 export const drizzle = singleton("drizzle", () =>
   drizzleVercelPostgresAdapter(db, { schema }),
 );
-
-// export const prisma = singleton(
-//   "prisma",
-//   () => new PrismaClient({ log: ["query", "info", "warn", "error"] }),
-// );
