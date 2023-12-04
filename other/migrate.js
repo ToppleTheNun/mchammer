856c7a357745aa9f45a53ada4f3f6f31371e67a6
Richard Harrah <topplethenunnery@gmail.com>
2023-12-04 00:45:40 -0500

    migrate is no longer typescript I guess

---
+++ other/migrate.js
@@ -0,0 +1,15 @@
+import process from "node:process";
+
+import { drizzle as drizzlePostgresJsAdapter } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+
+import postgres from "postgres";
+
+(async () => {
+  const pg = postgres(process.env.DATABASE_URL);
+  const drizzle = drizzlePostgresJsAdapter(pg);
+
+  console.log("Starting drizzle migrations...");
+  await migrate(drizzle, { migrationsFolder: "drizzle" });
+  console.log("Finished drizzle migrations!");
+})().catch(console.error);
--- other/migrate.ts
+++
@@ -1,9 +0,0 @@
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-
-import { pg } from "~/lib/storage.server.ts";
-
-(async () => {
-  console.log("Starting drizzle migrations...");
-  await migrate(pg, { migrationsFolder: "drizzle" });
-  console.log("Finished drizzle migrations!");
-})().catch(console.error);
